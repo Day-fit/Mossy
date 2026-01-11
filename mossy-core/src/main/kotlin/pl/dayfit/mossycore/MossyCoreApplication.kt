30c45896125f69834358b4c6f76d432df709c1c4
dayfit <xdayfit@gmail.com>
2026-01-11 14:45:51 +0100

    Initialize mossy-core module with Spring Boot project structure. Includes Maven configuration, Kotlin setup, and IntelliJ project files.

---
+++ mossy-core/src/main/kotlin/pl/dayfit/mossycore/MossyCoreApplication.kt
@@ -0,0 +1,11 @@
+package pl.dayfit.mossycore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MossyCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<MossyCoreApplication>(*args)
+}
---
+++ mossy-core/src/test/kotlin/pl/dayfit/mossycore/MossyCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.dayfit.mossycore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MossyCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
