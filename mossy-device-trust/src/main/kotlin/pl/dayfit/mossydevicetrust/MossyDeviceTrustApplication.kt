37362d7151494bf47ded4534783c05abd349a3f9
dayfit <xdayfit@gmail.com>
2026-07-07 17:11:02 +0200

    feat: Initialize mossy-device-trust project with basic configuration and structure

---
+++ mossy-device-trust/src/main/kotlin/pl/dayfit/mossydevicetrust/MossyDeviceTrustApplication.kt
@@ -0,0 +1,11 @@
+package pl.dayfit.mossydevicetrust
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MossyDeviceTrustApplication
+
+fun main(args: Array<String>) {
+    runApplication<MossyDeviceTrustApplication>(*args)
+}
---
+++ mossy-device-trust/src/test/kotlin/pl/dayfit/mossydevicetrust/MossyDeviceTrustApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.dayfit.mossydevicetrust
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MossyDeviceTrustApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
