93e0f0e754b65edb335a874e93df229b8dced21f
Day-fit <xdayfit@gmail.com>
2026-09-10 10:30:08 +0200

    refactor: move RabbitMQ queue declarations to definitions files
    - Removed inline queue declarations in RabbitMqConfiguration.
    - Added `mossy.email.queue` and `mossy.email.retry.queue` definitions to RabbitMQ JSON files.

--- mossy-mailer/src/main/kotlin/pl/dayfit/mossymailer/configuration/RabbitMqConfiguration.kt
+++ mossy-mailer/src/main/kotlin/pl/dayfit/mossymailer/configuration/RabbitMqConfiguration.kt
@@ -1,8 +1,6 @@
 package pl.dayfit.mossymailer.configuration
 
 import org.springframework.amqp.core.AcknowledgeMode
-import org.springframework.amqp.core.Queue
-import org.springframework.amqp.core.QueueBuilder
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.boot.amqp.autoconfigure.SimpleRabbitListenerContainerFactoryConfigurer
@@ -11,19 +9,6 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RabbitMqConfiguration {
-    @Bean
-    fun emailQueue(): Queue = QueueBuilder.durable("mossy.email.queue")
-        .deadLetterExchange("")
-        .deadLetterRoutingKey("mossy.email.retry.queue")
-        .build()
-
-    @Bean
-    fun emailRetryQueue(): Queue = QueueBuilder.durable("mossy.email.retry.queue")
-        .ttl(30_000)
-        .deadLetterExchange("")
-        .deadLetterRoutingKey("mossy.email.queue")
-        .build()
-
     @Bean
     fun rabbitListenerContainerFactory(
         configurer: SimpleRabbitListenerContainerFactoryConfigurer,
